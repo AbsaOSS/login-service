f3abf55b4ef0f47a079a56e1affb173f397ed6f1
Bartlomiej Baj <bajbartlomiej997@gmail.com>
2023-02-08 18:28:43 +0200

    Add unit tests for JWTService

---
+++ service/src/test/scala/za/co/absa/logingw/rest/service/JWTServiceTest.scala
@@ -0,0 +1,83 @@
+package za.co.absa.logingw.rest.service
+
+import io.jsonwebtoken.{Claims, Jws, Jwts}
+import org.scalatest.flatspec.AnyFlatSpec
+import za.co.absa.logingw.model.User
+
+import java.util
+import scala.util.Try
+
+class JWTServiceTest extends AnyFlatSpec {
+
+  private val jwtService: JWTService = new JWTService()
+
+  private val userWithoutGroups: User = User(
+    name = "testUser",
+    email = "test@gmail.com",
+    groups = Seq.empty
+  )
+
+  private val userWithGroups: User = userWithoutGroups.copy(
+    groups = Seq("testGroup1", "testGroup2")
+  )
+
+  private def parseJWT(jwt: String): Try[Jws[Claims]] = Try {
+    Jwts.parserBuilder().setSigningKey(jwtService.publicKey).build().parseClaimsJws(jwt)
+  }
+
+  behavior of "generateToken"
+
+  it should "return a JWT that is verifiable by `publicKey`" in {
+    val jwt = jwtService.generateToken(userWithoutGroups)
+    val parsedJWT = parseJWT(jwt)
+
+    assert(parsedJWT.isSuccess)
+  }
+
+  it should "return a JWT with subject equal to User.name" in {
+    val jwt = jwtService.generateToken(userWithoutGroups)
+    val parsedJWT = parseJWT(jwt)
+    val actualSubject = parsedJWT
+      .map(_.getBody.getSubject)
+      .get
+
+    assert(actualSubject === userWithoutGroups.name)
+  }
+
+  it should "return a JWT with email claim equal to User.email" in {
+    val jwt = jwtService.generateToken(userWithoutGroups)
+    val parsedJWT = parseJWT(jwt)
+    val actualSubject = parsedJWT
+      .map(_.getBody.get("email", classOf[String]))
+      .get
+
+    assert(actualSubject === userWithoutGroups.email)
+  }
+
+  it should "turn groups into empty `groups` claim for user without groups" in {
+    import scala.collection.JavaConverters._
+
+    val jwt = jwtService.generateToken(userWithoutGroups)
+    val parsedJWT = parseJWT(jwt)
+    val actualGroups = parsedJWT
+      .map(_.getBody.get("groups", classOf[util.ArrayList[String]]))
+      .get
+      .asScala
+
+    assert(actualGroups === userWithoutGroups.groups)
+  }
+
+  it should "turn groups into empty `groups` claim for user with groups" in {
+    import scala.collection.JavaConverters._
+
+    val jwt = jwtService.generateToken(userWithGroups)
+    val parsedJWT = parseJWT(jwt)
+    val actualGroups = parsedJWT
+      .map(_.getBody.get("groups", classOf[util.ArrayList[String]]))
+      .get
+      .asScala
+
+    assert(actualGroups === userWithGroups.groups)
+  }
+
+}
