e552ccbdc24a55c737a94709d67ccde3fb248ec3
TheLydonKing <Lydon.darocha@absa.africa>
2023-12-06 16:16:17 +0200

    Add License and let Project Build

--- examples/src/main/scala/za/co/absa/clientexample/Application.scala
+++ examples/src/main/scala/za/co/absa/clientexample/Application.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package za.co.absa.clientexample
 
 import za.co.absa.clientexample.config.ConfigProvider
@@ -8,7 +24,8 @@ import java.util.Scanner
 object Application {
   def main(args: Array[String]): Unit = {
 
-    var configPath = ""
+    //TODO: Add Client Client Library and uncomment code
+    /*var configPath = ""
     if (args.length < 1) {
       throw new Exception("Usage: Application <config_path>")
     } else {
@@ -80,6 +97,6 @@ object Application {
           println(s"UNAUTHORIZED. PLEASE TRY AGAIN")
           println("----------------------------------------------")
       }
-    }
+    }*/
   }
 }
