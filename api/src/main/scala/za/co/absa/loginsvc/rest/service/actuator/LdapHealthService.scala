f389af04035bb4389479059bb2f89cd9371f7941
Lydon da Rocha <Lydon.darocha@absa.africa>
2025-04-17 11:44:23 +0200

    Initial addition of ldap health code

---
+++ api/src/main/scala/za/co/absa/loginsvc/rest/service/actuator/LdapHealthService.scala
@@ -0,0 +1,43 @@
+package za.co.absa.loginsvc.rest.service.actuator
+
+import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.boot.actuate.health.{Health, HealthIndicator}
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+import za.co.absa.loginsvc.rest.config.provider.AuthConfigProvider
+
+import java.util
+import javax.naming.{Context, NamingException}
+import javax.naming.directory.{DirContext, InitialDirContext}
+
+@Component("ldapHealthIndicator")
+class LdapHealthService @Autowired() (authConfigsProvider: AuthConfigProvider) extends HealthIndicator {
+  override def health(): Health = {
+    val activeDirectoryLDAPConfig = authConfigsProvider.getLdapConfig
+    activeDirectoryLDAPConfig
+      .fold(Health.up().withDetail("reason", "ldap authentication not found in configuration, setting it to disabled").build()) (config =>
+        if(config.order <= 0)
+          {
+            Health.up().withDetail("reason", "ldap order parameter is set to 0, setting it to disabled").build()
+          }
+        else
+          {
+            val env = new util.Hashtable[String, String]
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
+            env.put(Context.PROVIDER_URL, config.url)
+            env.put(Context.SECURITY_AUTHENTICATION, "simple")
+            env.put(Context.SECURITY_PRINCIPAL, config.serviceAccount.username)
+            env.put(Context.SECURITY_CREDENTIALS, config.serviceAccount.password)
+
+            try {
+              val ctx: DirContext = new InitialDirContext(env)
+              ctx.close()
+              Health.up().withDetail("reason", s"Successfully connected to ${config.url}").build()
+            }
+            catch {
+              case e: NamingException =>
+                Health.down().withDetail("reason", s"Failed to connect: ${e.getMessage}").build()
+            }
+          })
+  }
+}
