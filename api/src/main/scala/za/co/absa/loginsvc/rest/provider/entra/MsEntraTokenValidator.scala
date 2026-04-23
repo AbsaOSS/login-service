a31cc31d423393764531a237f57e632fab9c77da
Daniel Kavan <Daniel.Kavan@absa.africa>
2026-04-23 14:22:17 +0200

    #161 RemoteJWKSet timeout attempt via DefaultResourceRetriever

--- api/src/main/scala/za/co/absa/loginsvc/rest/provider/entra/MsEntraTokenValidator.scala
+++ api/src/main/scala/za/co/absa/loginsvc/rest/provider/entra/MsEntraTokenValidator.scala
@@ -20,6 +20,7 @@ import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.jwk.source.{JWKSource, RemoteJWKSet}
 import com.nimbusds.jose.proc.{JWSVerificationKeySelector, SecurityContext => NimbusSecurityContext}
+import com.nimbusds.jose.util.DefaultResourceRetriever
 import com.nimbusds.jwt.proc.{BadJWTException, DefaultJWTClaimsVerifier, DefaultJWTProcessor}
 import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
 import org.slf4j.LoggerFactory
@@ -69,7 +70,10 @@ class MsEntraTokenValidator(
       .build(new CacheLoader[String, JWKSource[NimbusSecurityContext]] {
         override def load(jwksUri: String): JWKSource[NimbusSecurityContext] = {
           logger.info(s"Loading JWKS from $jwksUri")
-          new RemoteJWKSet[NimbusSecurityContext](new URL(jwksUri))
+          new RemoteJWKSet[NimbusSecurityContext](
+            new URL(jwksUri),
+            new DefaultResourceRetriever(10000, 10000) // 10s connect and read timeouts // TODO nicefy, this is just to evalute
+          )
         }
       })
 
